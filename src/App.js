e1c861307bd29a25dab4e2067c0f334fa9ae47b7
Andrea Donegà <andrea.donega.00@gmail.com>
2023-01-31 21:26:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='grid'>
        <Navbar />
+       <Home />
     </div>
   );
 }
